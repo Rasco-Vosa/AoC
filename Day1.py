541167e2408a9a08baca8f7b401589bae67ccb6e
Vasco Rosa <rascovosa@gmail.com>
2023-12-01 19:53:02 +0000

    First commit

---
+++ Day1.py
@@ -0,0 +1,13 @@
+rawData = open('Day1-input.txt','r').read()
+parsedData = rawData.splitlines()
+index_store = []
+calibrationValueSum = 0
+for txtLine in parsedData:
+    index_store = []
+    for i in range(0,len(txtLine)):
+        if (txtLine[i].isdigit()):
+            index_store.append(i)
+    calibrationValue = int(str(txtLine[min(index_store)]) + str(txtLine[max(index_store)]));
+    calibrationValueSum += calibrationValue
+        
+
