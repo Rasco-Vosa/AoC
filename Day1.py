95a935afc88d115f4542da565e10c06b7a8b30a7
Vasco Rosa <rascovosa@gmail.com>
2023-12-02 20:42:05 +0000

    Day2

--- Day1.py
+++ Day1.py
@@ -1,6 +1,5 @@
 rawData = open('Day1-input.txt','r').read()
 parsedData = rawData.splitlines()
-index_store = []
 calibrationValueSum = 0
 for txtLine in parsedData:
     index_store = []
@@ -9,5 +8,6 @@ for txtLine in parsedData:
             index_store.append(i)
     calibrationValue = int(str(txtLine[min(index_store)]) + str(txtLine[max(index_store)]));
     calibrationValueSum += calibrationValue
-        
+print("the final answer is: *Tatarara *" + str(calibrationValueSum))        
+
 
